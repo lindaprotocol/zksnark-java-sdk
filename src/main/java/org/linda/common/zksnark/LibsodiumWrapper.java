47e8e0f0b9dc32e0fa3063038a5963df32465c9b
lindaprotocol <lindaprotocol@gmail.com>
2025-11-17 19:45:46 +0330

    Fixed bug maven build zk-SNARK Java SDK implementation

---
+++ src/main/java/org/linda/common/zksnark/LibsodiumWrapper.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// package org.linda.common.zksnark;
+
+// import org.linda.common.util.Utils;
+
+// public class LibsodiumWrapper {
+//     private static final Libsodium INSTANCE = new Libsodium();
+
+//     static {
+//         Utils.LIBRARY.load();
+//     }
+
+//     private LibsodiumWrapper() throws IllegalAccessException
+//     {
+//         throw new IllegalAccessException();
+//     }
+
+//   public static Libsodium getInstance() {
+//     return INSTANCE;
+//   }
+
+// }
+package org.linda.common.zksnark;
+
+public class LibsodiumWrapper {
+    private static final Libsodium INSTANCE = new Libsodium();
+
+    private LibsodiumWrapper() throws IllegalAccessException {
+        throw new IllegalAccessException("LibsodiumWrapper is a utility class and cannot be instantiated");
+    }
+
+    public static Libsodium getInstance() {
+        return INSTANCE;
+    }
+}
